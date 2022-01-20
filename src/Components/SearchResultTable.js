71c9f159a506f90a56be11adf2900648851a1a14
ewan-spence <ewan_spence@btinternet.com>
2022-01-20 09:26:59 +0000

    Adds vscode to gitignore and shows dates in a readable format

--- src/Components/SearchResultTable.js
+++ src/Components/SearchResultTable.js
@@ -1,4 +1,5 @@
 import { isNullOrUndefined, isObject } from '@syncfusion/ej2-base';
+import moment from 'moment';
 import Table from 'react-bootstrap/Table';
 
 function SearchResultTable({ sections, results, tableStyle }) {
@@ -26,9 +27,20 @@ function SearchResultTable({ sections, results, tableStyle }) {
                         {resultsOfType.map(result => {
                             return <tr>
                                 {Object.values(result).map((value, index) => {
+                                    // Only show the name field of embedded objects
                                     if (isObject(value)) return <td>{value["name"]}</td>;
+
+                                    // Ignore any null values
                                     if (isNullOrUndefined(value)) return null;
+
+                                    // Ignore hard-to-read IDs
                                     if (Object.keys(result)[index].toLowerCase().includes("id")) return null;
+
+                                    // Format dates in a readable way
+                                    if (moment(value, moment.ISO_8601, true).isValid()) {
+                                        var dateValue = new Date(value);
+                                        return <td>{dateValue.toLocaleDateString()} {dateValue.toLocaleTimeString()}</td>;
+                                    }
 
                                     return <td>{value}</td>
                                 })}
